aab7d1ab1db722c1bf3aa2d43dd0703d79699ed4
Gharam almutairi <Gharamfaiham@gmail.com>
2020-10-25 19:02:34 +0300

    Create hello_world.py

---
+++ Python/hello_world.py
@@ -0,0 +1,16 @@
+# 1
+print( "Hello World" )
+# 2
+name = "gharam"
+print( "Hello",name)	# with a comma
+print( "Hello "+ name)	# with a +
+# 3
+name = 9
+print( "Hello ",name )	# with a comma
+print( " Hello  "+str(name))	
+
+# 4
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {} and {}.".format(fave_food1,fave_food2) ) # with .format()
+print( f"I love to eat {fave_food1 } and {fave_food2}.") # with an f string
