7d0a6dcf8a8ae09440dd07f5a7da556c0282c739
Gharam almutairi <Gharamfaihan@gmail.com>
2020-11-13 20:12:18 +0300

    Create GharamAlmutairi_Login_and_Registration.rar

--- python_stack/django/full_stack_django/login/loginapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('registration', views.registration)
-    ,path('login', views.login)
-    ]
